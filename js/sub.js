0b794f50ab2e4ba9ed7b23a2db2a8215a4f4fb6a
JungHeekyung <kerryjhk@gmail.com>
2024-06-16 12:58:22 +0900

    애니메이션 추가

--- js/sub.js
+++ js/sub.js
@@ -78,5 +78,133 @@ $(document).ready(function() {
         }
     });
 
-
 });
+
+gsap.registerPlugin(ScrollTrigger);
+
+const cont__ani = gsap.utils.toArray('.cont__ani');
+gsap.set(cont__ani, {transform: "translateY(50%)", opacity: 0 });
+cont__ani.forEach(cont__aniItem => {
+    gsap.to(cont__aniItem, {
+        transform: "translateY(0%)",
+        opacity: 1,
+        autoAlpha: 1,
+        duration: 0.5,
+        scrollTrigger: {
+            trigger: cont__aniItem,
+            start: 'top 80%',
+            toggleActions: "play none none none",
+        }
+    });
+});
+
+const cont__ani_slow = gsap.utils.toArray('.cont__ani_slow');
+gsap.set(cont__ani_slow, {transform: "translateY(30%)", opacity: 0 });
+cont__ani_slow.forEach(cont__ani_slowItem => {
+    gsap.to(cont__ani_slowItem, {
+        transform: "translateY(0%)",
+        opacity: 1,
+        autoAlpha: 1,
+        duration: 1,
+        scrollTrigger: {
+            trigger: cont__ani_slowItem,
+            start: 'top 80%',
+            toggleActions: "play none none none",
+        }
+    });
+});
+
+const cont__ani_delay = gsap.utils.toArray('.cont__ani_delay');
+gsap.set(cont__ani_delay, {transform: "translateY(50%)", opacity: 0 });
+cont__ani_delay.forEach(cont__ani_delayItem => {
+    gsap.to(cont__ani_delayItem, {
+        transform: "translateY(0%)",
+        opacity: 1,
+        autoAlpha: 1,
+        duration: 0.8,
+        delay: 0.3,
+        scrollTrigger: {
+            trigger: cont__ani_delayItem,
+            start: 'top 70%',
+            toggleActions: "play none none none",
+        }
+    });
+});
+
+document.querySelectorAll(".contents").forEach((section, index) => {
+    let tl = gsap.timeline({
+      scrollTrigger: {
+        trigger: ".contents .content-box",
+        start: "top +70%",
+        end: "top +=400",
+        toggleActions: "play none none none"
+      }
+    });
+  
+    tl.fromTo(
+      section.querySelectorAll(".box__ani1"),
+      { opacity: 0, transform: "translateY(50%)" },
+      { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+      "+=0.5"
+    );
+  
+    tl.fromTo(
+      section.querySelectorAll(".box__ani2"),
+      { opacity: 0, transform: "translateY(50%)" },
+      { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+      "-=0.3"
+    );
+    tl.fromTo(
+        section.querySelectorAll(".box__ani3"),
+        { opacity: 0, transform: "translateY(50%)" },
+        { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+        "-=0.3"
+      );
+      tl.fromTo(
+        section.querySelectorAll(".box__ani4"),
+        { opacity: 0, transform: "translateY(50%)" },
+        { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+        "-=0.3"
+      );
+  });
+
+
+
+document.querySelectorAll(".function").forEach((section, index) => {
+    let tl2 = gsap.timeline({
+      scrollTrigger: {
+        trigger: section,
+        start: "top +70%",
+        end: "top +=400",
+        toggleActions: "play none none none"
+      }
+    });
+  
+    tl2.fromTo(
+      section.querySelectorAll(".box__ani1"),
+      { opacity: 0, transform: "translateY(50%)" },
+      { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+      "+=0"
+    );
+  
+    tl2.fromTo(
+      section.querySelectorAll(".box__ani2"),
+      { opacity: 0, transform: "translateY(50%)" },
+      { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+      "-=0.5"
+    );
+  
+    tl2.fromTo(
+      section.querySelectorAll(".box__ani3"),
+      { opacity: 0, transform: "translateY(50%)" },
+      { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+      "-=0.5"
+    );
+  
+    tl2.fromTo(
+      section.querySelectorAll(".box__ani4"),
+      { opacity: 0, transform: "translateY(50%)" },
+      { opacity: 1, transform: "translateY(0%)", duration: 0.7 },
+      "-=0.5"
+    );
+  });
